716fe40aa5dce62d4373700cdc44ba00af6ff3d0
SohelShaikh_2712 <sohelshaikh2799@gmail.com>
2024-03-10 00:00:52 +0530

    first commit

---
+++ src/Com/Sohelshaikh/Main.java
@@ -0,0 +1,11 @@
+package Com.Sohelshaikh;
+
+public class Main {
+    public static void main(String[] args){
+        // Hello World program in java
+        // This java program prints hello world on the screen
+
+        System.out.println("Hello World!");
+
+    }
+}
